5c65a2099bbcfa4575a4d151c505687fec3604b6
Natali Avromov <natalia@coognitive.com>
2021-02-08 17:50:39 +0200

    .

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-export GITHUB_TOKEN=$PLUGIN_GITHUB_TOKEN
+#export GITHUB_TOKEN=$PLUGIN_GITHUB_TOKEN
 npx release-it --no-npm.publish $PLUGIN_RELEASE_TYPE --github.release --no-git.requireUpstream --ci
